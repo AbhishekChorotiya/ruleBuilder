7d60f5e89ec31e724c5b89b62f4bb08879c2b71b
Abhishek Chorotiya <abhishek.c@Abhishek-Chorotiya-QQL714W2Y3.local>
2025-06-07 03:56:55 +0530

    vite - react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex flex-col'>
+      <p className="text-lg flex justify-center p-4 bg-blue-100">
+        Rule Builder
+      </p>
+      <div className='w-full h-full'>
+      </div>
+    </div>
+  )
+}
+
+export default App
